121ffcf59d5e6a086797f508fe8396ffc8a5d4c6
Joshua Samuel <tweeterfriend@gmail.com>
2020-07-02 20:48:39 -0700

    mediums are too easy

---
+++ Top_K_Frequent_Elements/main.py
@@ -0,0 +1,33 @@
+"""Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order."""
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        freq = {}
+
+        for i in nums:
+            freq[i] = freq.get(i, 0) + 1
+
+        sorted_lst = [ky for ky,v in sorted(freq.items(), key = lambda item: -item[1])]
+
+        return sorted_lst[:1] if k == 1 else sorted_lst[:k]
