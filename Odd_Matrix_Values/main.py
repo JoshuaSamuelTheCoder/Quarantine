ed4a478d5e3cfc2546d8dcb4cafaacc90e82da59
Joshua Samuel <joshuasamuel@berkeley.edu>
2021-01-03 23:05:26 -0800

    learned something new about python matrix initialization

---
+++ Odd_Matrix_Values/main.py
@@ -0,0 +1,36 @@
+"""
+Given n and m which are the dimensions of a matrix initialized by zeros and given an array indices where indices[i] = [ri, ci]. For each pair of [ri, ci] you have to increment all cells in row ri and column ci by 1.
+
+Return the number of cells with odd values in the matrix after applying the increment to all indices.
+
+Example 1:
+Input: n = 2, m = 3, indices = [[0,1],[1,1]]
+Output: 6
+Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+After applying first increment it becomes [[1,2,1],[0,1,0]].
+The final matrix will be [[1,3,1],[1,3,1]] which contains 6 odd numbers.
+
+Example 2:
+Input: n = 2, m = 2, indices = [[1,1],[0,0]]
+Output: 0
+Explanation: Final matrix = [[2,2],[2,2]]. There is no odd number in the final matrix.
+"""
+
+class Solution(object):
+    def oddCells(self, n, m, indices):
+        """
+        :type n: int
+        :type m: int
+        :type indices: List[List[int]]
+        :rtype: int
+        """
+        mat = [[0]*m for i in range(n)]
+
+        for ind in indices:
+            y, x = ind
+            for i in range(n):
+                mat[i][x] += 1
+
+            for j in range(m):
+                mat[y][j] += 1
+        return sum(sum(i % 2 == 1 for i in j) for j in mat)
