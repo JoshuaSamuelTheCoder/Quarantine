daa5ff82b117dd30e24745a590a15573a67ab195
Joshua Samuel <joshuasamuel@berkeley.edu>
2021-01-13 22:05:02 -0800

    fun string sorting problem

---
+++ Increasing_Decreasing_Array/main.py
@@ -0,0 +1,68 @@
+"""
+Given a string s. You should re-order the string using the following algorithm:
+
+Pick the smallest character from s and append it to the result.
+Pick the smallest character from s which is greater than the last appended character to the result and append it.
+Repeat step 2 until you cannot pick more characters.
+Pick the largest character from s and append it to the result.
+Pick the largest character from s which is smaller than the last appended character to the result and append it.
+Repeat step 5 until you cannot pick more characters.
+Repeat the steps from 1 to 6 until you pick all characters from s.
+In each step, If the smallest or the largest character appears more than once you can choose any occurrence and append it to the result.
+
+Return the result string after sorting s with this algorithm.
+
+
+
+Example 1:
+
+Input: s = "aaaabbbbcccc"
+Output: "abccbaabccba"
+Explanation: After steps 1, 2 and 3 of the first iteration, result = "abc"
+After steps 4, 5 and 6 of the first iteration, result = "abccba"
+First iteration is done. Now s = "aabbcc" and we go back to step 1
+After steps 1, 2 and 3 of the second iteration, result = "abccbaabc"
+After steps 4, 5 and 6 of the second iteration, result = "abccbaabccba"
+Example 2:
+
+Input: s = "rat"
+Output: "art"
+Explanation: The word "rat" becomes "art" after re-ordering it with the mentioned algorithm.
+Example 3:
+
+Input: s = "leetcode"
+Output: "cdelotee"
+Example 4:
+
+Input: s = "ggggggg"
+Output: "ggggggg"
+Example 5:
+
+Input: s = "spo"
+Output: "ops"
+"""
+
+class Solution(object):
+    def sortString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        freq = {}
+        for c in s:
+            freq[c] = freq.get(c, 0) + 1
+
+        rtn_st = ""
+        r = False
+        while len(freq) > 0:
+            cur_keys = list(freq.keys())
+            cur_keys.sort(reverse=r)
+            r = not r
+            i = 0
+            for c in cur_keys:
+                rtn_st += c
+                freq[c] -= 1
+                if freq[c] == 0:
+                    del freq[c]
+
+        return rtn_st
