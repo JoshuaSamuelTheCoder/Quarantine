b369883d10b121e28eb4eae38b5060f4f29ac6ef
Joshua Samuel <joshuasamuel@berkeley.edu>
2021-02-07 14:40:12 -0800

    back to c, improvement in 9 month old solution

--- Longest_Substring_Without_Repeating_Characters/main.c
+++ Longest_Substring_Without_Repeating_Characters/main.c
@@ -18,128 +18,92 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
-int isUnique(char *s, int cheat) {
-
-    if(cheat < strlen(s)) {
-        return cheat;
-    }
-    for(int i = 0; i < strlen(s); i++) {
-        for(int j = i+1; j < strlen(s); j++) {
-            if(s[i] == s[j]) {
-                return j;
-            }
+//abcabcbb
+//abc, len = 3
+//Solution, Sliding Window. Hashmap to keep track of window letter frequency.
+// O(N) time complexity, O(N) space complexity
+int getKeys(char* map, int size) {
+    //O(N) operation :(
+    //obsolete
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if (map[i] > 0) {
+            count += 1;
         }
     }
-    return strlen(s) + 1;
-}
-
-int isUnique_2(char *s) {
-
-
-    for(int i = 0; i < strlen(s); i++) {
-        for(int j = i+1; j < strlen(s); j++) {
-            if(s[i] == s[j]) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-int lengthOfLongestSubstring_h1(char * s){
-
-    int len = 100;
-    len = strlen(s);
-    int maxLen = 0;
-    int isUn = len;
-    for(int i = 0; i < len; i++) {
-
-        for(int j = i; j < len; j++) {
-
-
-            if(j-i+1 <= maxLen ) {
-
-                continue;
-            }
-            char *example = malloc(j-i+2);
-
-
-            //example = &s[i];
-            memcpy(example, s+i, j-i+1);
-            example[j-i+1] = '\0';
-
-            //printf(example);
-
-            if(strlen(example) > maxLen) {
-                isUn = isUnique_2(example);
-                if(isUn) {
-                    maxLen = strlen(example);
-                }
-
-            }
-            //printf("%d is %d\n", strlen(example), isUn);
-            //printf(example);
-
-            free(example);
-
-            //printf("\n");
-        }
-    }
-
-    return maxLen;
+    return count;
 
 }
 
-
-int lengthOfLongestSubstring_h2(char * s){
-
-    int len = 100;
-    len = strlen(s);
-    int maxLen = 0;
-    int isUn = len;
-    for(int i = 0; i < len; i++) {
-
-        for(int j = len-1; j >= 0; j--) {
-
-
-            if(j-i+1 <= maxLen || isUn < j-i+1) {
-
-                continue;
-            }
-            char *example = malloc(j-i+2);
-
-
-            //example = &s[i];
-            memcpy(example, s+i, j-i+1);
-            example[j-i+1] = '\0';
-
-            //printf(example);
-
-            if(strlen(example) > maxLen) {
-                isUn = isUnique(example, isUn);
-                if(isUn > strlen(example)) {
-                    maxLen = strlen(example);
-                }
-
-            }
-            //printf("%d is %d\n", strlen(example), isUn);
-            //printf(example);
-
-            free(example);
-
-            //printf("\n");
-        }
-    }
-
-    return maxLen;
-
+int getIndex(char c) {
+    return c - '0' + 16;
 }
+
+int notValid(char c) {
+    return getIndex(c) < 0 || getIndex(c) > 100;
+}
+
 
 int lengthOfLongestSubstring(char * s){
 
-    if (strlen(s) > 1000) {
-        return lengthOfLongestSubstring_h2(s);
-    } else {
-        return lengthOfLongestSubstring_h1(s);
+    if (s == NULL || strlen(s) == 0) {
+        return 0;
     }
+
+    char *map = (char *) calloc(101, sizeof(char));
+
+    int left = 0;
+    int right = 0;
+
+    int k = 0;
+    while(k < strlen(s)) {
+
+        if(notValid(s[k])) {
+            return 1;
+        }
+
+        int index = getIndex(s[k]);
+
+        if (map[index] == 0) {
+            map[index] += 1;
+            k += 1;
+            right += 1;
+        } else {
+            break;
+        }
+    }
+
+    int maxLen = k;
+
+    while(right < strlen(s)) {
+
+        if(notValid(s[k])) {
+            return 1;
+        }
+
+        char st = s[k];
+        int index = getIndex(st);
+        if (map[index] > 0) {
+
+            if (right - left > maxLen) {
+                maxLen = right - left;
+            }
+            map[getIndex(s[left])] -= 1;
+            left += 1;
+        } else {
+            map[getIndex(s[right])] += 1;
+            right += 1;
+            k += 1;
+        }
+    }
+
+    if (left < strlen(s)) {
+        if (right - left > maxLen) {
+            maxLen = right - left;
+        }
+    }
+    free(map);
+
+    return maxLen;
+
 }
